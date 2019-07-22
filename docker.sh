0dfa4f017e6b2da5d01b7f13bc72620940060a38
Vitaly Uvarov <vitalyu@gmail.com>
2019-07-22 16:04:46 +0300

    docker.sh get current branch by default

--- docker.sh
+++ docker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -eu
 
-function usage(){ echo "Usage: $(basename $0) <build|push> <tag> [message]" && exit 1; }
+function usage(){ echo "Usage: $(basename $0) <build|push> [tag = current branch] [message]" && exit 1; }
 [ $# -lt 1 ] && usage;
 
 repo=dodopizza/centos
+current_branch=$( git branch | grep \* | cut -d ' ' -f2 )
 
 action=${1:-'build'}
-tag=${2:-'latest'}
+tag=${2:-${current_branch}}
 message=${1:-"${tag}"}
 
 echo "[~] Build with tag '${tag}'"
